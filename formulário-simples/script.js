ad8e7fa93d681149761d3b89d3755e20a5d50a1c
Cláudio Eduardo <102838108+eduxce@users.noreply.github.com>
2022-05-10 22:28:00 -0300

    Add files via upload

---
+++ formulário-simples/script.js
@@ -0,0 +1,11 @@
+var email = document.getElementById('email');
+
+email.addEventListener('focus', () => {
+    email.style.borderColor = "#ccc";
+});
+
+var senha = document.getElementById('senha');
+
+senha.addEventListener('focus', () => {
+    senha.style.borderColor = "#ccc";
+});
